b9b22c81c8ed4763088f0141e2a03fe4a01d23ef
Administrator <Administrator@lyd.mshome.net>
2017-12-15 10:15:39 +0800

    lyd

--- src/helloworld/Test.java
+++ src/helloworld/Test.java
@@ -3,6 +3,6 @@ package helloworld;
 public class Test {
 	public static void main(String[] args) {
 		System.out.println("我的第一个helloworld");
-		System.out.println("sss");
+		System.out.println("hello git");
 	}
 }
